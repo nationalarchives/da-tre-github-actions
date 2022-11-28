a498e65a2b6b44aa27c7eab5bd640e560f88405a
rahmedzaizi <rahmed@zaizi.com>
2022-11-28 19:07:09 +0000

    tf-plan-revie-apply yml file added

--- scripts/log.py
+++ scripts/log.py
@@ -56,5 +56,5 @@ def send_message_to_slack():
     if resp.status_code == 200:
         print(f'OK: {resp.json().get("ok")}, Error: {resp.json().get("error")}')
     else:
-        print(f'Fail to send: {resp.json().get("error")}')
+        print(f'Fail to send: {resp.json()}')
 send_message_to_slack()
