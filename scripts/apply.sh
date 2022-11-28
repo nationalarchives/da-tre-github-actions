3773cbcdd037e62a225b3b743e34e1fe3111118e
rahmedzaizi <rahmed@zaizi.com>
2022-11-28 19:48:09 +0000

    init script added

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -15,13 +15,7 @@ pwd
 cd "${TF_DIR}"
 pwd
 ls -la
-terraform -v
-aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_BACKEND_KEY}" --with-decryption --query "Parameters[*].Value" --output text > backend.conf 2> error.txt
-aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_VARS_KEY}" --with-decryption --query "Parameters[*].Value" --output text > terraform.tfvars 2> error.txt
-terraform init -backend-config=backend.conf -reconfigure > /dev/null 2> error.txt
-terraform workspace list
-terraform workspace select "${ENV}"
+sh ../scripts/init.sh
 terraform plan -var-file="terraform.tfvars" -input=false -out plan.out > /dev/null 2> error.txt
-aws s3 cp s3://dev-te-testdata/tmp/"${ENV}"/"${TRIGGERING_ACTOR}"/plan.out plan.out
-terraform show -no-color plan.out > plan.txt
+aws s3 cp "${TERRAFORM_PLAN_BUCKET}"/"${ENV}"/"${TRIGGERING_ACTOR}"/plan.out plan.out
 terraform apply -input=false plan.out > apply.out 2> error.txt
