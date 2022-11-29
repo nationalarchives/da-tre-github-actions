ee59366aad41f92b6caf7b3196fd426fa7399d21
rahmedzaizi <rahmed@zaizi.com>
2022-11-29 12:17:06 +0000

    fixed file path

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -15,8 +15,13 @@ pwd
 cd "${TF_DIR}"
 ls -la
 sh ../scripts/init.sh
+echo "4"
+ls -la
 terraform plan -var-file="terraform.tfvars" -input=false -out plan.out > /dev/null 2> error.txt
+echo "4"
+ls -la
 terraform show -no-color plan.out > plan.txt
+echo "5"
 ls -la
 echo "${TERRAFORM_PLAN_BUCKET}"
 aws s3 cp plan.out "${TERRAFORM_PLAN_BUCKET}"/"${ENV}"/"${TRIGGERING_ACTOR}"/
