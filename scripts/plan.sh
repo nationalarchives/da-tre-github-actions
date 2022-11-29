eb72426523d0d6354f35e946c647357c73d7979d
rahmedzaizi <rahmed@zaizi.com>
2022-11-29 11:55:23 +0000

    fixed file path

--- scripts/plan.sh
+++ scripts/plan.sh
@@ -5,7 +5,7 @@ set -e
 echo
 function cleanup {
     status=$?; 
-    if [ $status != 0 ] && [ -f error.txt ]; then echo "Failure: $status" && python3 ../scripts/send_to_cw.py error.txt; fi
+    if [ -f error.txt ]; then echo "Failure: $status" && python3 ../scripts/send_to_cw.py error.txt; fi
 }
 
 trap cleanup EXIT
@@ -17,4 +17,5 @@ ls -la
 sh ../scripts/init.sh
 terraform plan -var-file="terraform.tfvars" -input=false -out plan.out > /dev/null 2> error.txt
 terraform show -no-color plan.out > plan.txt
+ls -la
 aws s3 cp plan.out "${TERRAFORM_PLAN_BUCKET}"/"${ENV}"/"${TRIGGERING_ACTOR}"/plan.out
