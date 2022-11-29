bdd25bf7d77f76421a6d4945799839679e14e695
rahmedzaizi <rahmed@zaizi.com>
2022-11-29 13:40:45 +0000

    changed tfvars to json

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,7 +2,7 @@
 
 terraform -v
 aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_BACKEND_KEY}" --with-decryption --query "Parameters[*].Value" --output text > backend.conf 2> error.txt
-aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_VARS_KEY}" --with-decryption --query "Parameters[*].Value" --output text > terraform.tfvars 2> error.txt
+aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_VARS_KEY}" --with-decryption --query "Parameters[*].Value" --output text > terraform.tfvars.json 2> error.txt
 terraform init -backend-config=backend.conf -reconfigure > /dev/null 2> error.txt
 terraform workspace list > /dev/null 2> error.txt
 terraform workspace select "${ENV}" > /dev/null 2> error.txt
