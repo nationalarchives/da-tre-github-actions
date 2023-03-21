ac106acc8508f69eeb23e093843467d56f977fb3
rahmedzaizi <rahmed@zaizi.com>
2023-03-21 00:44:07 +0000

    pte test

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,11 +4,9 @@ terraform -v
 aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_BACKEND_KEY}" --with-decryption --query "Parameters[*].Value" --output text > backend.conf 2> error.txt
 aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_VARS_KEY}" --with-decryption --query "Parameters[*].Value" --output text > terraform.tfvars.json 2> error.txt
 terraform init -backend-config=backend.conf -reconfigure > /dev/null 2> error.txt
-terraform workspace list > /dev/null 2> error.txt
-if $(terraform workspace list | grep -q "${ENV}") ; then \
-    terraform workspace select ${ENV};
-    echo "Workspace is ${ENV}"
+terraform workspace list 2> error.txt
+if $(terraform workspace list 2> error.txt | grep -q "${TF_WORKSPACE}") ; then \
+    terraform workspace select ${TF_WORKSPACE} 2> error.txt;
 else 
-    terraform workspace new ${ENV};
-    echo "New workspace ${ENV} created"
+    terraform workspace new ${TF_WORKSPACE} 2> error.txt;
 fi
