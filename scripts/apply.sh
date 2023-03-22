52fb062bd7125447dc75e2237bf8099a0410b9e1
rahmedzaizi <rahmed@zaizi.com>
2023-03-22 10:53:28 +0000

    pte test

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd "${TF_DIR}"
-echo "test init-apply"
+ls -la ../
 sh ../scripts/init.sh
 aws s3 cp "${TERRAFORM_PLAN_BUCKET}"/"${TRE_ENV}"/"${TRIGGERING_ACTOR}"/plan.out plan.out > /dev/null 2> error.txt
 terraform apply -no-color -input=false plan.out > apply.out 2> error.txt
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 set -e
 terraform -v
-echo "test be-key"
 aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_BACKEND_KEY}" --with-decryption --query "Parameters[*].Value" --output text > backend.conf 2> error.txt
-echo "test key"
 aws ssm get-parameters --name "${AWS_PARAM_STORE_TF_VARS_KEY}" --with-decryption --query "Parameters[*].Value" --output text > terraform.tfvars.json 2> error.txt
-echo "test init"
 terraform init -backend-config=backend.conf -reconfigure > /dev/null 2> error.txt
-echo "test"
 terraform workspace list 2> error.txt
-echo "test2"
 if $(terraform workspace list 2> error.txt | grep -q "${TRE_ENV}") ; then \
     terraform workspace select ${TRE_ENV} 2> error.txt;
 else 
