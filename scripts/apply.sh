16f8dbc1bd468d90bf0523e370a23277130cc4e9
Ben S <122529331+ben-cerium@users.noreply.github.com>
2025-07-21 16:06:28 +0100

    fix: Restore errors to stdout

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -3,5 +3,5 @@ set -e
 cd "${TF_DIR}"
 ls -la ../
 sh ../scripts/init.sh
-aws s3 cp "${TERRAFORM_PLAN_BUCKET}"/"${TRE_ENV}"/"${TRIGGERING_ACTOR}"/plan.out plan.out > /dev/null 2> error.txt
-terraform apply -no-color -input=false plan.out > apply.out 2> error.txt
+aws s3 cp "${TERRAFORM_PLAN_BUCKET}"/"${TRE_ENV}"/"${TRIGGERING_ACTOR}"/plan.out plan.out
+terraform apply -no-color -input=false plan.out > apply.out
